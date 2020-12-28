ecb7505c491284085ffc7832ba4ad60ee5642385
YamTian <76648237+YamTian@users.noreply.github.com>
2020-12-28 12:08:04 +0800

    Update CamScanner_Pro.js

--- JaveScript/CamScanner_Pro.js
+++ JaveScript/CamScanner_Pro.js
@@ -1,5 +1,9 @@
 /*
+[Script]
 全能扫描王 Pro = type=http-response, pattern=^https:\/\/(api|api-cs)\.intsig\.net\/purchase\/cs\/query_property\?, requires-body=1, max-size=-1, script-path=https://raw.githubusercontent.com/YamTian/Network/master/JaveScript/CamScanner_Pro.js
+
+[Mitm]
+hostname = ap*.intsig.net
 */
 
 let obj = JSON.parse($response.body);
