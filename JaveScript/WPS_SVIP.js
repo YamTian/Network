415b580d1e4758a618bd78f9b2da1a256aec7913
YamTian <76648237+YamTian@users.noreply.github.com>
2020-12-28 12:51:50 +0800

    Update WPS_SVIP.js

--- JaveScript/WPS_SVIP.js
+++ JaveScript/WPS_SVIP.js
@@ -14,12 +14,12 @@ obj = {
   exp: 0,
   level: 3,
   privilege: [
-    { spid: "data_recover", times: 0, expire_time: 1694691709 },
-    { spid: "ocr", times: 0, expire_time: 1694691709 },
-    { spid: "pdf2doc", times: 0, expire_time: 1694691709 },
-    { spid: "pdf_merge", times: 0, expire_time: 1694691709 },
-    { spid: "pdf_sign", times: 0, expire_time: 1694691709 },
-    { spid: "pdf_split", times: 0, expire_time: 1694691709 }
+    { spid: "data_recover", times: 0, expire_time: 4102415999 },
+    { spid: "ocr", times: 0, expire_time: 4102415999 },
+    { spid: "pdf2doc", times: 0, expire_time: 4102415999 },
+    { spid: "pdf_merge", times: 0, expire_time: 4102415999 },
+    { spid: "pdf_sign", times: 0, expire_time: 4102415999 },
+    { spid: "pdf_split", times: 0, expire_time: 4102415999 }
   ],
   result: "ok",
   total_buy: 0,
@@ -29,15 +29,15 @@ obj = {
     name: "超级会员",
     has_ad: 0,
     memberid: 40,
-    expire_time: 1694691709,
+    expire_time: 4102415999,
     enabled: [
-      { memberid: 40, name: "超级会员", expire_time: 1694691709 },
-      { memberid: 20, name: "WPS会员", expire_time: 1694691709 },
-      { memberid: 12, name: "稻壳会员", expire_time: 1694691709 }
+      { memberid: 40, name: "超级会员", expire_time: 4102415999 },
+      { memberid: 20, name: "WPS会员", expire_time: 4102415999 },
+      { memberid: 12, name: "稻壳会员", expire_time: 4102415999 }
     ]
   },
   wealth: 0,
-  expire_time: 1694691709
+  expire_time: 4102415999
 };
 
 obj.userid = id;
