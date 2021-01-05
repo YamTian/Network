2a96e33df15e4ed57878f1337933ff80f1e12aa7
YamTian <zhaoben1998@icloud.com>
2021-01-06 00:35:31 +0800

    .

--- JaveScript/Leave_1.js
+++ JaveScript/Leave_1.js
@@ -4,13 +4,13 @@ var obj = JSON.parse($response.body);
     {
       "LeaveType": "事假", // 请假类型
       "WithNumNo": "0", // 同行人数
-      "OutAddress": "云南省昆明市",  // 请假去向
+      "OutAddress": "",  // 请假去向
       "FDYThing": "同意", // 同意请假
       "Status": "假期中", // 假期中、审批中
       "ID": null, // 随便4位数以获取别人的请假信息
-      "LeaveBeginDate": "2021-01-01", // 起始日期
+      "LeaveBeginDate": "2021-01-11", // 起始日期
       "LeaveBeginTime": "10", // 起始时间(小时)
-      "LeaveEndDate": "2021-01-01", // 结束日期
+      "LeaveEndDate": "2021-01-11", // 结束日期
       "LeaveEndTime": "22", // 结束时间(小时)
       "LeaveNumNo": 0.50, // 用请假时长/24的值
     }
--- JaveScript/Leave_2.js
+++ JaveScript/Leave_2.js
@@ -8,40 +8,30 @@ hostname = %APPEND% xg.kmmu.edu.cn
 
 var obj = JSON.parse($response.body);
  obj= {
-     // 以下为需要修改的假条信息
+    // 以下为需要修改的假条信息
+    // 请假内容(必填)
     "LeaveType": "事假", // 请假类型
-    "LeaveThing": "修手机", // 请假事由
-    "OutAddress": "云南省昆明市呈贡区七彩云南第一城", // 外出地点
-
-    "OutName": "", // 外出联系人信息-姓名
-    "OutMoveTel": "", // 外出联系人信息-移动电话
-    "OutTel": null, // 外出联系人信息-固定电话
-    "Relation": "", // 外出联系人信息-与本人关系
-
-    "StuMoveTel": "19969266266", // 本人信息-联系电话
-    "StuOtherTel": "", // 本人信息-其他联系方式
-
-    "ParentContacts": "", // 家长信息-家长联系人
-    "ParentTel": "", // 家长信息-家长联系方式
-
-    "LeaveBeginDate": "2021-01-11", // 往返交通工具-去-日期(三者需要相同)
-    "Inputdate": "2021-01-11", // 往返交通工具-去-日期(三者需要相同)
-    "GoDate": "2021-01-11", // 往返交通工具-去-日期(三者需要相同)
-
-    "LeaveBeginTime": "15",// 往返交通工具-去-整时(二者需要相同)
-    "GoTime": "15", // 往返交通工具-去-整时(二者需要相同)
-
-    "GoVehicle": "汽车", // 往返交通工具-去-交通工具(二者需要相同)
-
-    "LeaveEndDate": "2021-01-11", // 往返交通工具-返-日期(二者需要相同)
-    "BackDate": "2021-01-11", // 往返交通工具-返-日期(二者需要相同)
-
-    "LeaveEndTime": "20", // 往返交通工具-返-整时(二者需要相同)
-    "BackTime": "20", // 往返交通工具-返-整时(二者需要相同)
-
-    "BackVehicle": "汽车", // 往返交通工具-返-交通工具
-
-    // 以下数据不需要修改
+    "LeaveThing": "", // 请假事由
+    "OutAddress": "", // 外出地点
+    // 外出联系人信息(可不填)
+    "OutName": "", // 姓名
+    "OutMoveTel": "", // 移动电话
+    "OutTel": "", // 固定电话
+    "Relation": "", // 与本人关系
+    // 本人信息(必填)
+    "StuMoveTel": "", // 联系电话
+    "StuOtherTel": "", // 其他联系方式
+    // 家长信息(必填)
+    "ParentContacts": "", // 家长联系人
+    "ParentTel": "", // 家长联系方式
+    // 往返交通工具(必须修改)
+    "LeaveBeginDate": "2021-01-11", // 去-日期
+    "Inputdate": "2021-01-11", // 去-日期
+    "GoDate": "2021-01-11", // 去-日期
+    "LeaveEndDate": "2021-01-11", // 返-日期
+    "BackDate": "2021-01-11", // 返-日期
+    
+    // 以下数据不要随意修改
     "DisLeaveDate": null,
     "WithNumNo": 0,
     "FDYStatus": "2",
@@ -52,11 +42,20 @@ var obj = JSON.parse($response.body);
     "Status": "5",
     "GoOutConfirm": null,
     "StuName": null,
-    "studentId": "201805001",
+    "studentId": "202020020",
     "XYThing": null,
     "OverStatus": 1,
     "ID": 0001,
     "DisLeaveMen": null,
+
+    // 以下数据可按个人需求修改
+    "LeaveBeginTime": "10", // 去-整时
+    "GoTime": "10", // 去-整时
+    "LeaveEndTime": "22", // 返-整时
+    "BackTime": "22", // 返-整时
+    "GoVehicle": "汽车", // 去-交通工具
+    "BackVehicle": "汽车", // 返-交通工具
+
 
   };
 $done({body: JSON.stringify(obj)});
