c2d761dfcdecd18549013f985f9f5e39e3a4845f
YamTian <76648237+YamTian@users.noreply.github.com>
2021-01-08 00:11:11 +0800

    Update Leave.js

--- JaveScript/Leave.js
+++ JaveScript/Leave.js
@@ -1,8 +1,9 @@
-const outAddress = $persistentStore.read('OutAddress') || '北京市朝阳区'; 
-const leaveThing = $persistentStore.read('LeaveThing') || '外出购物'; 
-const studentTel = $persistentStore.read('StudentTel') || '13813800580'; 
-const parentName = $persistentStore.read('ParentName') || '李四'; 
-const parentTel = $persistentStore.read('ParentTel') || '13813800590'; 
+const OS = $persistentStore.read('OutAddress') || '北京市朝阳区'; 
+const LT = $persistentStore.read('LeaveThing') || '外出购物'; 
+const ST = $persistentStore.read('StudentTel') || '13813800580'; 
+const PN = $persistentStore.read('ParentName') || '李四'; 
+const PT = $persistentStore.read('ParentTel') || '13813800590'; 
+const WNN = $persistentStore.read('WithNumNo') || '0'; 
 const month = $persistentStore.read('Month') || '01'; 
 const day = $persistentStore.read('Day') || '01'; 
 
@@ -16,8 +17,8 @@ if (Url.indexOf('Edit') == -1) {
     "AllLeaveManages": [
       {
         "LeaveType": "事假", // 请假类型
-        "WithNumNo": "0", // 同行人数
-        "OutAddress": outAddress,  // 外出地点
+        "WithNumNo": WNN, // 同行人数
+        "OutAddress": OA,  // 外出地点
         "FDYThing": "同意", // 同意请假
         "Status": "假期中", // 假期中、审批中
         "ID": 1, // 随便4位数以获取别人的请假信息
@@ -35,19 +36,19 @@ else {
   Body= {
     // 请假内容(必填)
     "LeaveType": "事假", // 请假类型
-    "LeaveThing": leaveThing, // 请假事由
-    "OutAddress": outAddress, // 外出地点
+    "LeaveThing": LT, // 请假事由
+    "OutAddress": OA, // 外出地点
     // 外出联系人信息(可不填)
     "OutName": "", // 姓名
     "OutMoveTel": "", // 移动电话
     "OutTel": "", // 固定电话
     "Relation": "", // 与本人关系
     // 本人信息(必填)
-    "StuMoveTel": studentTel, // 联系电话
+    "StuMoveTel": ST, // 联系电话
     "StuOtherTel": "", // 其他联系方式
     // 家长信息(必填)
-    "ParentContacts": parentName, // 家长联系人
-    "ParentTel": parentTel, // 家长联系方式
+    "ParentContacts": PN, // 家长联系人
+    "ParentTel": PT, // 家长联系方式
     // 往返交通工具(必须修改)
     "LeaveBeginDate": Today, // 去-日期
     "Inputdate": Today, // 去-日期
@@ -57,7 +58,7 @@ else {
       
     // 以下数据不要随意修改
     "DisLeaveDate": null,
-    "WithNumNo": 0,
+    "WithNumNo": WNN,
     "FDYStatus": "2",
     "LeaveNumNo": 1.00,
     "GoOut": "1",
