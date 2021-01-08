fb1179e2d2f977e8007d090d211f280926459291
YamTian <zhaoben1998@icloud.com>
2021-01-08 17:44:02 +0800

    .

--- JaveScript/Leave.js
+++ JaveScript/Leave.js
@@ -1,69 +1,75 @@
-const month = $persistentStore.read('Month') || ''; 
-const day = $persistentStore.read('Day') || ''; 
-const leaveType = $persistentStore.read('LeaveType') || '事假'; 
-const leaveThing = $persistentStore.read('LeaveThing') || ''; 
-const outAddress = $persistentStore.read('OutAddress') || ''; 
-const studentTel = $persistentStore.read('StudentTel') || ''; 
-const parentName = $persistentStore.read('ParentName') || ''; 
-const relation = $persistentStore.read('Relation') || ''; 
-const parentTel = $persistentStore.read('ParentTel') || ''; 
-const withNumNo = $persistentStore.read('WithNumNo') || '0'; 
-const vehicle = $persistentStore.read('Vehicle') || '汽车'; 
+// 从 BoxJs 里面获取请假起始日期和结束日期
+const beginDay = $persistentStore.read('BeginDay') || ''; // 请假起始日期
+const endDay = $persistentStore.read('EndDay') || ''; // 请假结束日期
 
-var Year = "2021";
-var Today = Year + "-" + month + "-" + day;
+// 获取当前月份
+var myDate = new Date();
+var newMonth = myDate.getMonth() + 1; // 用myDate.getMonth()获取的1月份输出是0
+
+// 月份补零
+if (newMonth <= 10) {
+  newMonth = "0" + newMonth
+} else {
+  newMonth = newMonth
+}
+var Year = "2021"; // 2021年
+
+var BeginDate = Year + "-" + newMonth + "-" + beginDay; // 组合请假起始时间
+var EndDate = Year + "-" + newMonth + "-" + endDay; // 组合请假结束时间
+var LeaveNumNo = (endDay - beginDay + 0.50).toFixed(2); // 请假时长的计算
+
 var Url = $request.url;
 var Body = JSON.parse($response.body);
 
-if (Url.indexOf('Edit') == -1) {
+if (Url.indexOf('Edit') == -1) { // 响应体 Url 不包含 Edit
   Body= {
     "AllLeaveManages": [
       {
-        "LeaveType": leaveType, // 请假类型
-        "WithNumNo": withNumNo, // 同行人数
-        "OutAddress": outAddress,  // 外出地点
+        "LeaveType": $persistentStore.read('LeaveType') || '事假', // 请假类型
+        "WithNumNo": $persistentStore.read('WithNumNo') || '0', // 同行人数
+        "OutAddress": $persistentStore.read('OutAddress') || '',  // 外出地点
         "FDYThing": "同意", // 同意请假
         "Status": "假期中", // 假期中、审批中
         "ID": 1, // 随便4位数以获取别人的请假信息
-        "LeaveBeginDate": Today, // 去-日期
+        "LeaveBeginDate": BeginDate, // 去-日期
         "LeaveBeginTime": "10", // 去-整时
-        "LeaveEndDate": Today, // 返-日期
+        "LeaveEndDate": EndDate, // 返-日期
         "LeaveEndTime": "22", // 返-整时
-        "LeaveNumNo": 0.50, // 用请假时长/24的值(手动填写)
+        "LeaveNumNo": LeaveNumNo, // 用请假时长/24的值(手动填写)
       }
     ],
     "IsLeave": 1 // 是否在请假状态
   };
 }
-else {
+else { // 响应体 Url 包含 Edit
   Body= {
     // 请假内容
-    "LeaveType": leaveType, // 请假类型
-    "LeaveThing": leaveThing, // 请假事由
-    "OutAddress": outAddress, // 外出地点
+    "LeaveType": $persistentStore.read('LeaveType') || '事假', // 请假类型
+    "LeaveThing": $persistentStore.read('LeaveThing') || '', // 请假事由
+    "OutAddress": $persistentStore.read('OutAddress') || '', // 外出地点
     // 外出联系人信息(可不填)
-    "OutName": parentName, // 姓名
-    "OutMoveTel": parentTel, // 移动电话
+    "OutName": $persistentStore.read('ParentName') || '', // 姓名
+    "OutMoveTel": $persistentStore.read('ParentTel') || '', // 移动电话
     "OutTel": "", // 固定电话
-    "Relation": relation, // 与本人关系
+    "Relation": $persistentStore.read('Relation') || '', // 与本人关系
     // 本人信息
-    "StuMoveTel": studentTel, // 联系电话
+    "StuMoveTel": $persistentStore.read('StudentTel') || '', // 联系电话
     "StuOtherTel": "", // 其他联系方式
     // 家长信息
-    "ParentContacts": parentName, // 家长联系人
-    "ParentTel": parentTel, // 家长联系方式
+    "ParentContacts": $persistentStore.read('ParentName') || '', // 家长联系人
+    "ParentTel": $persistentStore.read('ParentTel') || '', // 家长联系方式
     // 往返时间
-    "LeaveBeginDate": Today, // 去-日期
-    "Inputdate": Today, // 去-日期
-    "GoDate": Today, // 去-日期
-    "LeaveEndDate": Today, // 返-日期
-    "BackDate": Today, // 返-日期
+    "LeaveBeginDate": BeginDate, // 去-日期
+    "Inputdate": BeginDate, // 去-日期
+    "GoDate": BeginDate, // 去-日期
+    "LeaveEndDate": EndDate, // 返-日期
+    "BackDate": EndDate, // 返-日期
       
     // 以下数据不可修改
     "DisLeaveDate": null,
-    "WithNumNo": withNumNo, // 同行人数
+    "WithNumNo": $persistentStore.read('WithNumNo') || '0', // 同行人数
     "FDYStatus": "2",
-    "LeaveNumNo": 1.00, // 用请假时长/24的值(自动生成)
+    "LeaveNumNo": LeaveNumNo, // 用请假时长/24的值(自动生成)
     "GoOut": "1", // 是否外出离校
     "FDYThing": "同意",
     "SpStatus": null,
@@ -81,8 +87,8 @@ else {
     "GoTime": "10", // 去-整时
     "LeaveEndTime": "22", // 返-整时
     "BackTime": "22", // 返-整时
-    "GoVehicle": vehicle, // 去-交通工具
-    "BackVehicle": vehicle, // 返-交通工具
+    "GoVehicle": $persistentStore.read('Vehicle') || '汽车', // 去-交通工具
+    "BackVehicle": $persistentStore.read('Vehicle') || '汽车', // 返-交通工具
   };
 }
 $done({body: JSON.stringify(Body)});
