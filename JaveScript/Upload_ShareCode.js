16bc3a474eb092eeac85f81aa56226512d865a35
YamTian <76648237+YamTian@users.noreply.github.com>
2021-01-10 00:50:05 +0800

    Update Upload_ShareCode.js

--- JaveScript/Upload_ShareCode.js
+++ JaveScript/Upload_ShareCode.js
@@ -3,7 +3,7 @@
 上传互助码 = type=cron,cronexp="0 0 6 1,10,20 * *", wake-system=1, timeout=180, script-path=https://raw.githubusercontent.com/YamTian/Network/master/JaveScript/Upload_ShareCode.js
 */
 
-const $ = new Env(userName)
+const $ = new Env('')
 const userName = $persistentStore.read('UserName') || '';
 const fruitCode = $persistentStore.read('FruitCode') || '';
 const petCode = $persistentStore.read('PetCode') || '';
@@ -163,7 +163,8 @@ function uploadCarzyJoy() {
 // 输出弹窗
 function showMsg() {
   return new Promise((resolve) => {
-    $.msg($.name, "", $.result.join('\n'));
+    // $.msg($.name, "", $.result.join('\n'));
+    $.msg("京东帐号:" + userName , "", $.result.join('\n'));
     resolve();
   });
 }
