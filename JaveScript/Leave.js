99f9a79dba34df0f52b2859938786ddb52206c0d
YamTian <76648237+YamTian@users.noreply.github.com>
2021-02-26 01:45:35 +0800

    Update Leave.js

--- JaveScript/Leave.js
+++ JaveScript/Leave.js
@@ -50,11 +50,11 @@ if (begin_date <= end_date) { // 否
 
 // 判断起始日期和结束日期是否跨月份
 if (begin_date <= end_date) { // 否
-  var LeaveNumNo = (end_date_q - begin_date_q + end_hours/24 - begin_hours/24).toFixed(2); // 计算请假总时长并保留两位小数
+  var LeaveNumNo = (end_date - begin_date + end_hours/24 - begin_hours/24).toFixed(2); // 计算请假总时长并保留两位小数
 } else { // 是
   var D = new Date(Year, Month, 0);
   var month_total_days = D.getDate(); // 获取当前月份总天数
-  var LeaveNumNo = (end_date_q - begin_date_q + month_total_days + end_hours/24 - begin_hours/24).toFixed(2); // 计算请假总时长并保留两位小数
+  var LeaveNumNo = (end_date - begin_date + month_total_days + end_hours/24 - begin_hours/24).toFixed(2); // 计算请假总时长并保留两位小数
 };
 
 var BeginMonth = ('0' + begin_month).slice(-2); // 起始月份补零
